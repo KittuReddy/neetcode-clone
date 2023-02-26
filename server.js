09f7c984154928b043278034a1780e59f9b2cbb8
Uday <uday160120737116@gmail.com>
2023-02-26 10:57:30 +0530

    Added FrontEnd

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("frontend"))
+
+app.get("/html",function(req,res){
+    res.sendFile(__dirname+"/frontend/SimpleHtml.html")
+})
+
 app.get('/', function(req, res){
-    res.send("Hello World");
+    res.send("Hello World")
+})
+
+app.get('/myName', function(req, res){
+    res.send("Krishna");
 })
 
 app.listen(3000, function(){
