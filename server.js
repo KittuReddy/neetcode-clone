d6f7bd634f53519b36af69f6de77b22792c6eaf6
Uday <uday160120737116@gmail.com>
2023-02-26 14:15:14 +0530

    Successfully Conneted To The Mongo DataBase

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const mongoose=require('mongoose');
+const dotenv=require("dotenv");
+dotenv.config();
 const express = require('express');
 const app = express();
+
 
 app.use(express.static("frontend"))
 
@@ -7,9 +11,25 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/frontend/SimpleHtml.html")
 })
 
-app.listen(3000, function(){
-    console.log("running on http://localhost:3000");
+mongoose.set('strictQuery',true);
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,function(err){
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log("DataBase Connection Successful");
+        app.listen(3000, function(){
+            console.log("running on http://localhost:3000");
+        })
+    }
 })
+
+// app.listen(3000, function(){
+//     console.log("running on http://localhost:3000");
+// })
 
 
 
